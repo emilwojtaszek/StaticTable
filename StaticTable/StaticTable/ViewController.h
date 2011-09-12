a2736c4062fc0615236f3823144b7f43554ce6a8
emil.wojtaszek <emil.wojtaszek@gmail.com>
2011-09-12 11:46:45 +0200

    add clean project

---
+++ StaticTable/StaticTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StaticTable
+//
+//  Created by Emil Wojtaszek on 12.09.2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
